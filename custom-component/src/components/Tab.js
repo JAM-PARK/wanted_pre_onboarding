32711436e232527e13a981f7e7df50bf41354aaf
JAM-PARK <chwk05@gmail.com>
2022-02-08 00:09:32 +0900

    Add : Tab feature

--- custom-component/src/components/Tab.js
+++ custom-component/src/components/Tab.js
@@ -2,14 +2,34 @@ import './Tab.scss';
 import { useState } from 'react';
 
 const Tab = () => {
+  const [curTabIdx, setCurTabIdx] = useState(0);
+
+  const tabArr = [
+    { menu: 'Tab1', desc: 'Tab menu ONE' },
+    { menu: 'Tab2', desc: 'Tab menu TWO' },
+    { menu: 'Tab3', desc: 'Tab menu THREE' },
+    { menu: 'Tab4', desc: 'Tab menu FOUR' },
+    { menu: 'Tab5', desc: 'Tab menu FIVE' },
+  ];
+
+  const handleTabIdx = (idx) => {
+    setCurTabIdx(idx);
+  };
+
   return (
     <div className="tab-container">
       <ul className="tab-menu">
-        <li className="submenu">test</li>
-        <li className="submenu">test2</li>
-        <li className="submenu">test3</li>
+        {tabArr.map((tab, idx) => (
+          <li
+            key={idx}
+            className={curTabIdx === idx ? 'sub-menu focus' : 'sub-menu'}
+            onClick={() => handleTabIdx(idx)}
+          >
+            {tab.menu}
+          </li>
+        ))}
       </ul>
-      <span className="tag-desc">test content</span>
+      <span className="tag-desc">{tabArr[curTabIdx].desc}</span>
     </div>
   );
 };
