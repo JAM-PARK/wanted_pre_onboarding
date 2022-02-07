ad89115451e783aa774d733d28065ab1f2b480c2
JAM-PARK <chwk05@gmail.com>
2022-02-07 21:31:23 +0900

    Add : Modal feature

--- custom-component/src/components/Modal.js
+++ custom-component/src/components/Modal.js
@@ -2,18 +2,29 @@ import './Modal.scss';
 import { useState } from 'react';
 
 const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <div className="modal-container">
-        <button className="modal-btn">Open Modal</button>
-        {/* <div className="modal-backdrop">
+    <div className="modal-container">
+      <button className="modal-btn" onClick={handleModal}>
+        Open Modal
+      </button>
+      {isOpen ? (
+        <div className="modal-backdrop">
           <div className="modal-view">
-            <span className="close-btn">&times;</span>
+            <span className="close-btn" onClick={handleModal}>
+              &times;
+            </span>
             <div className="desc">{`HELLO \n`}wanted &times; CODESTATES!</div>
           </div>
-        </div> */}
-      </div>
-    </>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
   );
 };
 
