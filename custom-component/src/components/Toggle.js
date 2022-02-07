7fa8894d0c6056c9fc60e45ccd922c0eaf32a498
JAM-PARK <chwk05@gmail.com>
2022-02-07 17:25:37 +0900

    Add : Toggle feature

--- custom-component/src/components/Toggle.js
+++ custom-component/src/components/Toggle.js
@@ -1,14 +1,21 @@
 import './Toggle.scss';
+import { useState } from 'react';
 
 const Toggle = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+  };
+
   return (
     <>
-      <div className="container">
-        <div className="toggle-container toggle-checked" />
+      <div className="container" onClick={handleToggle}>
+        <div className={`toggle-container ${isOn ? 'toggle-checked' : ''}`} />
         <div className="toggle-circle-container">
-          <div className="toggle-circle toggle-checked" />
+          <div className={`toggle-circle ${isOn ? 'toggle-checked' : ''}`} />
         </div>
-        <span className="desc">Toggle Switch ON</span>
+        <span className="desc">{isOn ? 'Toggle Switch ON' : 'Toggle Switch OFF'}</span>
       </div>
     </>
   );
