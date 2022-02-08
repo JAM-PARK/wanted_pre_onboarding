c13b0d034fceca29a05a1efd886cfc5e64526a48
JAM-PARK <chwk05@gmail.com>
2022-02-09 01:33:17 +0900

    Add : Tag featrue

--- custom-component/src/components/Tag.js
+++ custom-component/src/components/Tag.js
@@ -2,24 +2,39 @@ import './Tag.scss';
 import { useState } from 'react';
 
 const Tag = () => {
+  const initialTags = ['wanted', 'CODESTATES', '프리온보딩', '프론트엔드'];
+  const [tags, setTags] = useState(initialTags);
+
+  const deleteTags = (idxToDelete) => {
+    setTags(tags.filter((_, idx) => idx !== idxToDelete));
+  };
+
+  const addTags = (e) => {
+    const filteredTags = tags.filter((el) => el === e.target.value);
+    if (e.target.value !== '' && filteredTags.length === 0) {
+      setTags([...tags, e.target.value]);
+      e.target.value = '';
+    }
+  };
+
   return (
     <div className="tag-container">
       <div className="tag-input">
         <ul>
-          <li className="tag">
-            <span>test1</span>
-            <span className="tag-close-icon">&times;</span>
-          </li>
-          <li className="tag">
-            <span>test2</span>
-            <span className="tag-close-icon">&times;</span>
-          </li>
-          <li className="tag">
-            <span>test3</span>
-            <span className="tag-close-icon">&times;</span>
-          </li>
+          {tags.map((tag, idx) => (
+            <li key={idx} className="tag">
+              <span>{tag}</span>
+              <span className="tag-close-icon" onClick={() => deleteTags(idx)}>
+                &times;
+              </span>
+            </li>
+          ))}
         </ul>
-        <input type="text" placeholder="Press enter to add tag" />
+        <input
+          type="text"
+          onKeyUp={(e) => (e.key === 'Enter' ? addTags(e) : '')}
+          placeholder="Press enter to add tag"
+        />
       </div>
     </div>
   );
