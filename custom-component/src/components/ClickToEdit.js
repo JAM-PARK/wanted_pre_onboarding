d52c8e2e9b77f1c451a09c6942e719dfccd1c392
JAM-PARK <chwk05@gmail.com>
2022-02-10 15:58:10 +0900

    Add : Clicktoedit feature

--- custom-component/src/components/ClickToEdit.js
+++ custom-component/src/components/ClickToEdit.js
@@ -1,24 +1,77 @@
 import './ClickToEdit.scss';
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
+
+const InputEdit = ({ value, handleValueChange }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedValue, setEditedValue] = useState(value);
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    if (isEdit) {
+      inputEl.current.focus();
+    }
+  }, [isEdit]);
+
+  const handleBlur = () => {
+    setIsEdit(false);
+    handleValueChange(editedValue);
+  };
+
+  const handleInputChange = (e) => {
+    setEditedValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setIsEdit(!isEdit);
+  };
+
+  return (
+    <div className="input-box">
+      {isEdit ? (
+        <input
+          className="input-edit"
+          type="text"
+          ref={inputEl}
+          value={editedValue}
+          onBlur={handleBlur}
+          onChange={handleInputChange}
+          onKeyUp={(e) => (e.key === 'Enter' ? handleBlur() : '')}
+        />
+      ) : (
+        <span onClick={handleClick}>{editedValue}</span>
+      )}
+    </div>
+  );
+};
 
 const ClickToEdit = () => {
+  const initialInfo = {
+    name: '김코딩',
+    age: 30,
+  };
+
+  const [name, setName] = useState(initialInfo.name);
+  const [age, setAge] = useState(initialInfo.age);
+
   return (
     <div className="cte-container">
       <div className="input-section">
         <span>이름</span>
-        <div className="input-box">
-          <input className="input-edit" type="text"></input>
-          {/* <span>test</span> */}
-        </div>
+        <InputEdit
+          value={name}
+          handleValueChange={(changedValue) => setName(changedValue)}
+        />
       </div>
       <div className="input-section">
         <span>나이</span>
-        <div className="input-box">
-          <input className="input-edit" type="text"></input>
-          {/* <span>test</span> */}
-        </div>
+        <InputEdit
+          value={age}
+          handleValueChange={(changedValue) => setAge(changedValue)}
+        />
       </div>
-      <span>이름 김코딩 나이 22</span>
+      <span>
+        이름 {name} 나이 {age}
+      </span>
     </div>
   );
 };
